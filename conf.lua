62271974b48875e343f4d80beb7e78ece46b6162
wolfboyft <henryfthomson@gmail.com>
2022-04-10 22:16:56 +0100

    title

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 local consts = require("consts")
 
 function love.conf(t)
+	t.window.title = consts.title
 	t.window.width = consts.contentWidth * consts.contentScale
 	t.window.height = consts.contentHeight * consts.contentScale
 end
