5fe0aab851bea5cde70977b1aad96f04745b0c97
wolfboyft <henryfthomson@gmail.com>
2022-04-13 23:00:40 +0100

    bugfix

--- util/tryInteraction.lua
+++ util/tryInteraction.lua
@@ -1,10 +1,12 @@
 local util = require("util")
 
+local registry = require("registry")
+
 local function tryInteraction(world, entity)
 	local interactionTileX, interactionTileY = util.translateByDirection(entity.x, entity.y, entity.direction)
 	local interactees = util.getStationaryEntitiesAtTile(world, interactionTileX, interactionTileY, entity)
 	for _, interactee in ipairs(interactees) do
-		if interactee.type.door then
+		if registry.entityTypes[interactee.typeName].door then
 			interactee.open = not interactee.open
 		end
 	end
