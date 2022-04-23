58b9297ec1e749ff64368e823b025eae7716dc67
wolfboyft <henryfthomson@gmail.com>
2022-04-23 19:18:19 +0100

    Made all the furniture removable and placeable and renamed some entities

--- util/getMatchingRecipes.lua
+++ util/getMatchingRecipes.lua
@@ -7,44 +7,46 @@ local function getMatchingRecipes(stacks, recipeClassNames)
 	end
 	for _, recipeClassName in ipairs(recipeClassNames) do
 		local recipeClass = registry.recipeClasses[recipeClassName]
-		for _, recipe in ipairs(recipeClass) do
-			-- setup recipe with 0 items met
-			local recipeItemMatchingProfile = {}
-			-- match items with recipe
-			local doesntMatch = false
-			for _, stack in ipairs(stacks) do
-				local matchesAny = false
-				for _, recipeStack in ipairs(recipe.reagents) do
-					-- check if it matches (excluding count)
-					local matches = recipeStack.type == stack.type
-					if matches then
-						matchesAny = true
-						recipeItemMatchingProfile[recipeStack] = (recipeItemMatchingProfile[recipeStack] or 0) + stack.count
+		if recipeClass then
+			for _, recipe in ipairs(recipeClass) do
+				-- setup recipe with 0 items met
+				local recipeItemMatchingProfile = {}
+				-- match items with recipe
+				local doesntMatch = false
+				for _, stack in ipairs(stacks) do
+					local matchesAny = false
+					for _, recipeStack in ipairs(recipe.reagents) do
+						-- check if it matches (excluding count)
+						local matches = recipeStack.type == stack.type
+						if matches then
+							matchesAny = true
+							recipeItemMatchingProfile[recipeStack] = (recipeItemMatchingProfile[recipeStack] or 0) + stack.count
+							break
+						end
+					end
+					if not matchesAny then
+						-- if there are stacks that don't match the recipe, then the recipe isn't matched
+						doesntMatch = true
 						break
 					end
 				end
-				if not matchesAny then
-					-- if there are stacks that don't match the recipe, then the recipe isn't matched
-					doesntMatch = true
-					break
+				-- if there are reagents that aren't matched in the recipe, then the recipe isn't matched
+				for _, recipeStack in ipairs(recipe.reagents) do
+					local profile = recipeItemMatchingProfile[recipeStack]
+					if not profile or profile <= 0 then
+						doesntMatch = true
+					end
 				end
-			end
-			-- if there are reagents that aren't matched in the recipe, then the recipe isn't matched
-			for _, recipeStack in ipairs(recipe.reagents) do
-				local profile = recipeItemMatchingProfile[recipeStack]
-				if not profile or profile <= 0 then
-					doesntMatch = true
-				end
-			end
-			if not doesntMatch then
-				-- get maximum amount craftable and add to return table if it's >= 1
-				local maxAmount
-				for recipeStack, count in pairs(recipeItemMatchingProfile) do
-					local countThisStack = math.floor(count / recipeStack.count)
-					maxAmount = maxAmount and math.min(maxAmount, countThisStack) or countThisStack
-				end
-				if maxAmount >= 1 then
-					ret[#ret+1] = {recipe = recipe, type = recipe.product.type, count = recipe.product.count, maxAmount = maxAmount}
+				if not doesntMatch then
+					-- get maximum amount craftable and add to return table if it's >= 1
+					local maxAmount
+					for recipeStack, count in pairs(recipeItemMatchingProfile) do
+						local countThisStack = math.floor(count / recipeStack.count)
+						maxAmount = maxAmount and math.min(maxAmount, countThisStack) or countThisStack
+					end
+					if maxAmount >= 1 then
+						ret[#ret+1] = {recipe = recipe, type = recipe.product.type, count = recipe.product.count, maxAmount = maxAmount}
+					end
 				end
 			end
 		end
